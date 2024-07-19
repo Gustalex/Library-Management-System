5640ed291ac35c36b88b8523d378d920e5c38a9d
Gustavo Santos <alexandregustavo00@gmail.com>
2024-07-19 14:19:33 -0300

    chore: refatora a view de book

--- libSys/book/urls.py
+++ libSys/book/urls.py
@@ -11,10 +11,6 @@ router.register(r'genre', GenreViewSet, basename='genre')
 
 urlpatterns = [     
     re_path(r'^', include(router.urls)),
-    path('book/<int:pk>/reserve/', BookViewSet.as_view({'put': 'reserve_book'}), name='reserve_book'),
-    path('book/<int:pk>/borrow/', BookViewSet.as_view({'put': 'borrow_book'}), name='borrow_book'),
-    path('book/<int:pk>/return/', BookViewSet.as_view({'put': 'return_book'}), name='return_book'),
-    path('book/<int:pk>/borrow_reserved/', BookViewSet.as_view({'put': 'borrow_reserved_book'}), name='borrow_reserved_book'),
 ]
 
 
