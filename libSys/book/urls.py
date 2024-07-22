6c492408af6a642f148b92e90d4828b595aa3202
Gustavo Santos <alexandregustavo00@gmail.com>
2024-07-22 07:42:35 -0300

    chore: retira as views de livros inutilizadas

--- libSys/book/urls.py
+++ libSys/book/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from book.views import BookViewSet, GenreViewSet, UpdateStatusBookViewSet
+from book.views import (BookViewSet, GenreViewSet)
 
 router = DefaultRouter()
 router.register(r'book', BookViewSet, basename='book')
 router.register(r'genre', GenreViewSet, basename='genre')
-router.register(r'book-status', UpdateStatusBookViewSet, basename='book-status')
 
 urlpatterns = [
     path('', include(router.urls)),
