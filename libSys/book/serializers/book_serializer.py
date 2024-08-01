f7b93ac89392ad43788891ad59d116107fb6ca84
Gustavo Santos <alexandregustavo00@gmail.com>
2024-07-31 21:26:36 -0300

    fix: atualiza o serializer de livro para mostrar as informações corretamente

--- libSys/book/serializers/book_serializer.py
+++ libSys/book/serializers/book_serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-
 from book.models import Book
+from book.models.cover import Cover
+from book.serializers.genre_serializer import GenreSerializer
+
+class CoverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cover
+        fields = ['cover_image']
 
 class BookSerializer(serializers.ModelSerializer):
+    covers = CoverSerializer(many=True, read_only=True)
+    genre = GenreSerializer(read_only=True)
+
     class Meta:
         model = Book
         fields = '__all__'
@@ -10,9 +19,7 @@ class BookSerializer(serializers.ModelSerializer):
             'isbn': {'validators': []},
         }
 
-
 class UpdateStatusSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Book
-        fields=[]
-
+        model = Book
+        fields = []
